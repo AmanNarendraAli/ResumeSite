80510032f7f6c7863c37688f165db0d3b5255f45
AmanNarendraAli <amannarendraali@gmail.com>
2024-07-23 23:52:14 +0530

    31:49

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './Navbar.module.css';
 import {getImageUrl} from '../../utils';
 export const Navbar = () => {
+    const [menuOpen,setMenuOpen] = useState(false);
+
    return (
         <nav className = {styles.navbar}>
         <a className = {styles.title} href="/">Portfolio</a>
             <div className = {styles.menu}>
-                <img className = {styles.menuBtn} src = {getImageUrl("nav/menuIcon.png")} alt = "menu"/>
-                <ul className = {styles.menuItems}>
-                    <li><a href="#about">About</a></li>
+                <img className = {styles.menuBtn} src = {menuOpen ? "/assets/nav/closeIcon.png" : "/assets/nav/menuIcon.png"} alt = "menuButton" onClick = {()=>setMenuOpen(!menuOpen)}/>
+                <ul className = {`${styles.menuItems}${menuOpen && styles.menuOpen}`}> {/*if menuOpen is true, add styles.menuOpen*/}
+                <li><a href="#about">About</a></li>
                     <li><a href="#skills">Experience</a></li>
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
