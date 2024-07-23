a9094b3a1d194ed0e54f286c71129a448f9c7935
AmanNarendraAli <amannarendraali@gmail.com>
2024-07-23 23:18:58 +0530

    24:52

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,19 @@
 import React from "react";
-
+import styles from './Navbar.module.css';
+import {getImageUrl} from '../../utils';
 export const Navbar = () => {
-    return <div>NavBar</div>;
+   return (
+        <nav className = {styles.navbar}>
+        <a className = {styles.title} href="/">Portfolio</a>
+            <div className = {styles.menu}>
+                <img className = {styles.menuBtn} src = {getImageUrl("nav/menuIcon.png")} alt = "menu"/>
+                <ul className = {styles.menuItems}>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#skills">Experience</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+   );
 };
