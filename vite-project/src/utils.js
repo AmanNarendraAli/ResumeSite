5f0a31cca7d7e2498ee73c71c26f04a08c311449
AmanNarendraAli <amannarendraali@gmail.com>
2024-08-01 22:41:06 +0530

    fixed giu for real this time

--- vite-project/src/utils.js
+++ vite-project/src/utils.js
@@ -1,3 +1,5 @@
-export const getImageUrl = (image) => {
-  return new URL("/assets/$(path)", import.meta.url).href;
+export const getImageUrl = (imagePath) => {
+  const url = new URL(`/assets/${imagePath}`, import.meta.url).href;
+  console.log("Image URL:", url);
+  return url;
 };
