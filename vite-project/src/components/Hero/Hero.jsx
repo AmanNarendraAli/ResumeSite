1e7603b4c689ce7a9c45af1ead301d4d540c8bf7
AmanNarendraAli <amannarendraali@gmail.com>
2024-09-06 22:39:09 +0530

    altered desc

--- vite-project/src/components/Hero/Hero.jsx
+++ vite-project/src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I'm Aman</h1>
-                <p className={styles.description}>I'm an ML and full-stack developer. I graduated from Ashoka University in 2024 with a Magna Cum Laude in Computer Science. Reach out to learn more!</p>
+                <p className={styles.description}>I'm an ML developer who's learning full-stack development. I graduated from Ashoka University in 2024 with a Magna Cum Laude in Computer Science. Reach out to learn more!</p>
                 <a className={styles.contactBtn} href="mailto:myemail@email.com">Contact Me</a>
             </div>
             <img src={getImageUrl("hero/gradphoto.jpeg")} alt="me" className={styles.heroImg}/>
